2697292b5845781e0841811c457da6f50ec63011
andreikwork0 <andreikwork0@gmail.com>
2022-09-01 17:00:20 +0500

    company index init blade

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -13,7 +14,7 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        //
+        return view('company.index', ['companies' => Company::paginate(15)]);
     }
 
     /**
--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,1 +1,36 @@
-<?php
+@extends('layouts/app')
+
+@section('title-block') Организации @endsection
+
+@section('content')
+    <div style="padding: 50px 0;">
+    <div class="container">
+        <table class="table table-success table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($companies as $company)
+                <tr>
+                    <th scope="row">{{$company->id}}</th>
+                    <td>{{$company->name}}</td>
+                    <td>{{$company->legal_adress}}</td>
+                    <td>{{$company->mng_surname}}</td>
+                </tr>
+
+            @endforeach
+
+
+            </tbody>
+        </table>
+    </div>
+
+        <div class="d-flex">{{$companies->links()}}</div>
+    </div>
+@endsection
+
