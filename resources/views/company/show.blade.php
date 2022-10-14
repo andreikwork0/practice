48f544dfbc6778156d75a6d842ac3ce170ded065
andreikwork0 <andreikwork0@gmail.com>
2022-10-14 16:10:09 +0500

    fd

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -3,26 +3,26 @@
 @section('title-block') Организации {{ $company->name}}  @endsection
 
 @section('page-title')
-    <x-page-title> Организация {{ $company->name}} </x-page-title>
+    <x-page-title> {{$company->name}}  </x-page-title>
 @endsection
 
 @section('content')
 
     <div class="container">
-        <h2 class="mb-3">{{$company->name}}</h2>
-        <h3 class="mb-3">{{$company->name_full ?? ''}}</h3>
+
+        <h2 class="mb-3">{{$company->name_full ?? ''}}</h2>
 
         <div class="d-flex">
             <p class="mx-3"> Руководитель:  {{$company->mng_surname ?? ''}}  {{$company->mng_name ?? ''}}  {{$company->mng_patronymic ?? ''}}</p>
         </div>
 
-        <h5>Реквизиты</h5>
+        <h3>Реквизиты</h3>
         <div class="d-flex">
             <p class="mx-3">ИНН:   {{$company->inn }}</p>
             <p class="mx-3">КПП:   {{$company->kpp }}</p>
         </div>
 
-        <h5>Адрес</h5>
+        <h3>Адрес</h3>
         <div class="d-flex">
             <p class="mx-3"> Юридический адрес: {{$company->legal_adress ?? '-'}} </p>
             @if($company->fact_adress)
